19d78e2db97babdda094b318f4241b4d908ff51a
Brandon Vasquez <thatsjaskett@gmail.com>
2023-02-21 13:46:17 +0100

    Milestone 1 completata

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+    $length = $_GET['pass-length'] ?? 0;
+
+    //var_dump($length);
+
+    function generateRanPass($length) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!?$?%^&*()_-+={[}]:;@#|\<,>.?/';
+
+        $lengthChars = strlen($characters);
+
+        $randomPass = '';
+
+        for($i = 0; $i < $length; $i++) {
+            $randomPass .= $characters[rand(0, $lengthChars - 1)];
+        }
+
+        return $randomPass;
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +27,14 @@
     <title>PHP Strong Password Generator</title>
 </head>
 <body>
-    
+    <form>
+        <label for="pass-length">Scegli la lunghezza della password</label>
+        <input type="number" name="pass-length" min="7" max="15" placeholder="Minino 7 caratteri, massimo 15 caratteri">
+        <input type="submit" value="Genera">
+    </form>
+
+    <span>
+        La password generata è: <?php echo generateRanPass($length) ?>
+    </span>
 </body>
 </html>
